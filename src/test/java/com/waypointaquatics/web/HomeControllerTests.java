b194ebaa1d36e2d793ceef2c38006d28924baab1
Jon Steele <jonwhsteele@gmail.com>
2023-06-06 15:34:32 -0700

    Adds full application context to tests

--- src/test/java/com/waypointaquatics/web/HomeControllerTests.java
+++ src/test/java/com/waypointaquatics/web/HomeControllerTests.java
@@ -3,14 +3,14 @@ package com.waypointaquatics.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.waypointaquatics.web.home.HomeController;
 
-@WebMvcTest(HomeController.class)
+@SpringBootTest
 @AutoConfigureMockMvc
 public class HomeControllerTests {
 
